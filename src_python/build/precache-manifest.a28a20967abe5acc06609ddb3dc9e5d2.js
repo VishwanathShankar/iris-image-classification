1e7e4695d3bd9448c209871a55356f1ce211585e
Manasa R <manasar1@in.ibm.com>
2020-01-03 09:33:10 +0530

    Adding React code in build folder

---
+++ src_python/build/precache-manifest.a28a20967abe5acc06609ddb3dc9e5d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd89819f98200fcf1d0b69fc90ddc18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c98f9aa1c24df9572da1",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "aa567308c8d3f6440c97",
+    "url": "/static/css/main.390c33b7.chunk.css"
+  },
+  {
+    "revision": "c98f9aa1c24df9572da1",
+    "url": "/static/js/2.31c99279.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.31c99279.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa567308c8d3f6440c97",
+    "url": "/static/js/main.b009e1ae.chunk.js"
+  },
+  {
+    "revision": "4ff4644e6f863f7e70ac",
+    "url": "/static/js/runtime-main.63711e55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
